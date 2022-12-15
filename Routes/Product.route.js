4927109ab91f3652f0ee49c86f8376ba3c7cba76
Rishav Neogi <nrishav31@gmail.com>
2022-12-15 20:56:25 +0530

    limit modified

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -9,8 +9,7 @@ product.get("/",async(req,res)=>{
 
 product.get("/:type",async(req,res)=>{
     const types=req.params.type;
-    const {category,page}=req.query;
-    const limit=18;
+    const {category,page,limit=18}=req.query;
     if(category){
         res.send(await ProductModel.find({type:types,category:category}).limit(limit).skip((page-1)*limit));
     }
