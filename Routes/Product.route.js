44728b1acea003fc8110cb933b8c97b08c75e79a
Rishav Neogi <nrishav31@gmail.com>
2022-12-15 19:12:33 +0530

    fiter added

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -9,7 +9,14 @@ product.get("/",async(req,res)=>{
 
 product.get("/:type",async(req,res)=>{
     const types=req.params.type;
-    res.send(await ProductModel.find({type:types}));
+    const category=req.query.category;
+    console.log(types,category)
+    if(category){
+        res.send(await ProductModel.find({type:types,category:category}));
+    }
+    else{
+        res.send(await ProductModel.find({type:types}));
+    }
 });
 
 module.exports=product;
