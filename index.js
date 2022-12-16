a1767d10578e49b0c4a68af2341c061fdcfcb792
Rishav Neogi <nrishav31@gmail.com>
2022-12-16 15:28:48 +0530

    auth added in cart and wish

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const user = require("./Routes/User.route");
 const product = require("./Routes/Product.route");
 const wish = require("./Routes/Wish.route");
 const cart = require("./Routes/Cart.route");
+const auth = require("./Middlewares/Auth.middleware");
 app.use(express.json());
 app.use(cors({
   origin:"*"
@@ -20,8 +21,6 @@ app.use(cors({
 app.use("/api",API);
 app.use("/user",user);
 app.use("/product",product);
-app.use("/wish",wish);
-app.use("/cart",cart);
 
 app.post("/signup", async (req, res) => {
     try {
@@ -75,7 +74,9 @@ app.post("/signup", async (req, res) => {
       res.status(404).send({ msg: "Failed to login" });
     }
   });
-
+  app.use(auth)
+  app.use("/wish",wish);
+  app.use("/cart",cart);
 app.listen(port,()=>{
     try {
         database_connection;
